85caae1ac1362068fb6ad5c19b5217fa471e7f52
saida <saida.fathallah@gmail.com>
2023-04-04 23:36:43 +0200

    New Theme

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,21 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'outils_formation' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'wordpress' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'wordpress' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'database' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
